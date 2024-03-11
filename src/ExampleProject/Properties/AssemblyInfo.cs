c2f731617c3a965a6cc24b55d5ba12cfd14a6d46
KidiXDev <dikhiardian55@gmail.com>
2024-03-12 06:07:00 +0700

    change into .net standart 2.0

--- src/ExampleProject/Properties/AssemblyInfo.cs
+++ src/ExampleProject/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExampleProject")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © KidiXDev  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
